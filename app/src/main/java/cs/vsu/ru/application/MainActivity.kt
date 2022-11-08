3ab30a1bb18a85b720ee2c2bc9d45a50c475394b
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2022-11-08 11:43:44 +0300

    Add toolbar burger listener

--- app/src/main/java/cs/vsu/ru/application/MainActivity.kt
+++ app/src/main/java/cs/vsu/ru/application/MainActivity.kt
@@ -1,19 +1,24 @@
 package cs.vsu.ru.application
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout.DrawerListener
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.motion.widget.MotionScene
 import cs.vsu.ru.application.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var motion: MotionScene
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val actionBarDrawerToggle = object : ActionBarDrawerToggle(this, binding.root, R.string.drawer_open, R.string.drawer_close) {
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
@@ -25,10 +30,8 @@ class MainActivity : AppCompatActivity() {
         binding.root.addDrawerListener(actionBarDrawerToggle)
         actionBarDrawerToggle.syncState()
 
-        setContentView(binding.root)
         setViews()
 
-//
 //        binding.mainBackgroundImg.setBackgroundResource(R.drawable.background_daytime)
 //        binding.mainBackgroundImg.setAltImageResource(R.color.black)
     }
--- app/src/main/java/cs/vsu/ru/application/ui/main/MainFragment.kt
+++ app/src/main/java/cs/vsu/ru/application/ui/main/MainFragment.kt
@@ -1,9 +1,12 @@
 package cs.vsu.ru.application.ui.main
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
@@ -24,6 +27,13 @@ class MainFragment : Fragment() {
         binding = FragmentContentMainBinding.inflate(inflater)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         connectFragmentToViewModel()
+
+        binding.mainToolbarBurger.setOnClickListener {
+            activity?.findViewById<DrawerLayout>(R.id.activity_main_layout)?.openDrawer(GravityCompat.START)
+        }
+
+        val textView: TextView? = activity?.findViewById<TextView>(R.id.main_temperature_now_tv)
+        textView?.text = "65"
 
         return binding.root
     }
