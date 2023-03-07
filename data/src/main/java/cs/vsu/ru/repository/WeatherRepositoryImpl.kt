f69ddb63a95b8cb347b8a1774d0fefabc9d2ea84
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-08 01:17:07 +0300

    Refactor weather repository implementation

--- data/src/main/java/cs/vsu/ru/repository/WeatherRepositoryImpl.kt
+++ data/src/main/java/cs/vsu/ru/repository/WeatherRepositoryImpl.kt
@@ -4,12 +4,20 @@ import cs.vsu.ru.domain.model.location.Location
 import cs.vsu.ru.domain.model.weather.Weather
 import cs.vsu.ru.domain.repository.weather.WeatherRepository
 import cs.vsu.ru.environment.WeatherServiceProvider
+import cs.vsu.ru.mapper.WeatherMapper
 
-class WeatherRepositoryImpl(private val serviceProvider: WeatherServiceProvider) : WeatherRepository {
+class WeatherRepositoryImpl(
+    private val serviceProvider: WeatherServiceProvider,
+    private val mapper: WeatherMapper
+) : WeatherRepository {
 
     override suspend fun getWeather(location: Location): Weather {
-        val service = serviceProvider.weatherService
-        TODO("Make coroutine call")
-        //return Weather()
+        return mapper.toEntity(
+            serviceProvider.weatherService.getForecast(
+                latitude = location.latitude,
+                longitude = location.longitude,
+                key = serviceProvider.openWeatherApiKey
+            )
+        )
     }
 }
