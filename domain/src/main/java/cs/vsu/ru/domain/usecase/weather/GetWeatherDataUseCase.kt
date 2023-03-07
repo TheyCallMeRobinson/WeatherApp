e952abca3a6f8ad67a433964ba79b961735451aa
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-08 01:36:49 +0300

    Fix weather use case location issue

--- domain/src/main/java/cs/vsu/ru/domain/usecase/weather/GetWeatherDataUseCase.kt
+++ domain/src/main/java/cs/vsu/ru/domain/usecase/weather/GetWeatherDataUseCase.kt
@@ -1,11 +1,12 @@
 package cs.vsu.ru.domain.usecase.weather
 
+import cs.vsu.ru.domain.model.location.Location
 import cs.vsu.ru.domain.model.weather.Weather
 import cs.vsu.ru.domain.repository.weather.WeatherRepository
 
 class GetWeatherDataUseCase(val repository: WeatherRepository) {
 
-    fun execute(): Weather {
-        return repository.getWeather()
+    suspend fun execute(location: Location): Weather {
+        return repository.getWeather(location)
     }
 }
