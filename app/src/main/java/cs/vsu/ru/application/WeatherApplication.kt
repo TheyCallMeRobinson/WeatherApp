664d41290a367cee0c58cf32612b4d9e19286513
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-08 12:09:01 +0300

    Create koin start di in application class

--- app/src/main/java/cs/vsu/ru/application/WeatherApplication.kt
+++ app/src/main/java/cs/vsu/ru/application/WeatherApplication.kt
@@ -1,12 +1,23 @@
 package cs.vsu.ru.application
 
 import android.app.Application
-import android.arch.persistence.room.Room
-import cs.vsu.ru.database.AppDatabase
+import cs.vsu.ru.application.di.appModule
+import cs.vsu.ru.application.di.dataModule
+import cs.vsu.ru.application.di.domainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
 class WeatherApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        startKoin {
+            androidLogger(Level.DEBUG)
+            androidContext(applicationContext)
+            modules(listOf(appModule, dataModule, domainModule))
+        }
     }
 }
