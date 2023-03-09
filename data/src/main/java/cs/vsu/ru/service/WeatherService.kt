25a05b7487eec725382b9eaafbf34fb3136e14dc
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-09 03:07:42 +0300

    Connect to api server and successfully get 401 response code

--- data/src/main/java/cs/vsu/ru/service/WeatherService.kt
+++ data/src/main/java/cs/vsu/ru/service/WeatherService.kt
@@ -1,17 +1,18 @@
 package cs.vsu.ru.service
 
+import cs.vsu.ru.environment.WeatherServiceProvider
 import cs.vsu.ru.model.weather.WeatherFullResponse
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherService {
 
-    @GET
+    @GET(".")
     suspend fun getForecast(
         @Query("lat") latitude: Double?,
         @Query("lon") longitude: Double?,
         @Query(value = "exclude", encoded = true) exclude: String? = "current,minutely,alerts",
-        @Query("appid") key: String,
+        @Query("appid") key: String = WeatherServiceProvider.openWeatherApiKey,
         @Query("units") units: String? = "metric",
         @Query("lang") language: String? = "ru"
     ): WeatherFullResponse
