414660fcccaf4b2e0851651f06ee5b59134acf7c
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-10 13:22:12 +0300

    Create daily weather list

--- app/src/main/java/cs/vsu/ru/application/view/main/MainFragment.kt
+++ app/src/main/java/cs/vsu/ru/application/view/main/MainFragment.kt
@@ -1,9 +1,11 @@
 package cs.vsu.ru.application.view.main
 
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
 import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
@@ -13,12 +15,16 @@ import cs.vsu.ru.application.R
 import cs.vsu.ru.application.databinding.ContentMainBodyListBinding
 import cs.vsu.ru.application.databinding.ContentMainHeaderBinding
 import cs.vsu.ru.application.databinding.FragmentContentMainBinding
+import cs.vsu.ru.application.databinding.ItemDailyWeatherBinding
+import cs.vsu.ru.application.model.HumidityModel
 import cs.vsu.ru.application.model.TemperatureModel
 import cs.vsu.ru.application.view.adapter.HourlyListAdapter
 import cs.vsu.ru.application.viewmodel.MainViewModel
+import cs.vsu.ru.domain.model.weather.DailyWeather
 import cs.vsu.ru.domain.model.weather.Weather
 import cs.vsu.ru.environment.Status
 import org.koin.androidx.viewmodel.ext.android.viewModel
+import java.util.*
 
 
 class MainFragment : Fragment() {
@@ -92,6 +98,11 @@ class MainFragment : Fragment() {
     }
 
     private fun setMainBodyListValues(layout: ContentMainBodyListBinding, weather: Weather?) {
+        setHourlyTemperatureList(layout, weather)
+        setDailyWeatherList(weather?.dailyWeather!!)
+    }
+
+    private fun setHourlyTemperatureList(layout: ContentMainBodyListBinding, weather: Weather?) {
         val hourlyListAdapter = weather?.hourlyWeather?.let { HourlyListAdapter(it, weather.timezoneOffset) }
         val hourlyList = layout.hourlyTemperatureListContainer.hourlyTemperatureList
         val linearLayoutManager = LinearLayoutManager(context)
@@ -99,5 +110,21 @@ class MainFragment : Fragment() {
         hourlyList.layoutManager = linearLayoutManager
         hourlyList.adapter = hourlyListAdapter
         hourlyList.setHasFixedSize(true)
+    }
+
+    private fun setDailyWeatherList(dailyWeatherList: List<DailyWeather>) {
+        val dailyWeatherTable = binding.mainBodyList.dailyWeatherListContainer.dailyWeatherList
+
+        for (item in dailyWeatherList) {
+            val tableRow = TableRow(context)
+            val tableRowViewBinding = ItemDailyWeatherBinding.inflate(layoutInflater)
+            tableRowViewBinding.dayOfWeek.text = DateFormat.format("EEEE", Date(item.date * 1000L)).toString()
+            tableRowViewBinding.humidityHolder.itemHumidity.text = HumidityModel(item.humidity).toString()
+            tableRowViewBinding.itemDayTemperature.text = TemperatureModel(item.temperature.minTemperature).toString()
+            tableRowViewBinding.itemNightTemperature.text = TemperatureModel(item.temperature.maxTemperature).toString()
+            tableRow.addView(tableRowViewBinding.root)
+
+            dailyWeatherTable.addView(tableRow)
+        }
     }
 }
