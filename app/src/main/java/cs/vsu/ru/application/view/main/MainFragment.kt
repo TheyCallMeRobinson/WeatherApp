c9c67c3ae193d893a874468f37b3f43235e4fd93
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-10 14:09:44 +0300

    Create sunrise sunset view

--- app/src/main/java/cs/vsu/ru/application/view/main/MainFragment.kt
+++ app/src/main/java/cs/vsu/ru/application/view/main/MainFragment.kt
@@ -20,6 +20,7 @@ import cs.vsu.ru.application.model.HumidityModel
 import cs.vsu.ru.application.model.TemperatureModel
 import cs.vsu.ru.application.view.adapter.HourlyListAdapter
 import cs.vsu.ru.application.viewmodel.MainViewModel
+import cs.vsu.ru.domain.model.weather.CurrentWeather
 import cs.vsu.ru.domain.model.weather.DailyWeather
 import cs.vsu.ru.domain.model.weather.Weather
 import cs.vsu.ru.environment.Status
@@ -100,6 +101,7 @@ class MainFragment : Fragment() {
     private fun setMainBodyListValues(layout: ContentMainBodyListBinding, weather: Weather?) {
         setHourlyTemperatureList(layout, weather)
         setDailyWeatherList(weather?.dailyWeather!!)
+        setSunsetSunriseTime(layout, weather.currentWeather)
     }
 
     private fun setHourlyTemperatureList(layout: ContentMainBodyListBinding, weather: Weather?) {
@@ -118,13 +120,25 @@ class MainFragment : Fragment() {
         for (item in dailyWeatherList) {
             val tableRow = TableRow(context)
             val tableRowViewBinding = ItemDailyWeatherBinding.inflate(layoutInflater)
-            tableRowViewBinding.dayOfWeek.text = DateFormat.format("EEEE", Date(item.date * 1000L)).toString()
-            tableRowViewBinding.humidityHolder.itemHumidity.text = HumidityModel(item.humidity).toString()
-            tableRowViewBinding.itemDayTemperature.text = TemperatureModel(item.temperature.minTemperature).toString()
-            tableRowViewBinding.itemNightTemperature.text = TemperatureModel(item.temperature.maxTemperature).toString()
+
+            tableRowViewBinding.dayOfWeek.text =
+                DateFormat.format("EEEE", Date(item.date * 1000L)).toString()
+            tableRowViewBinding.humidityHolder.itemHumidity.text =
+                HumidityModel(item.humidity).toString()
+            tableRowViewBinding.itemDayTemperature.text =
+                TemperatureModel(item.temperature.minTemperature).toString()
+            tableRowViewBinding.itemNightTemperature.text =
+                TemperatureModel(item.temperature.maxTemperature).toString()
             tableRow.addView(tableRowViewBinding.root)
 
             dailyWeatherTable.addView(tableRow)
         }
+    }
+
+    private fun setSunsetSunriseTime(layout: ContentMainBodyListBinding, currentWeather: CurrentWeather) {
+        layout.sunriseSunsetHolderContainer.sunriseTimeTv.text =
+            DateFormat.format("HH:mm", Date(currentWeather.sunrise * 1000L)).toString()
+        layout.sunriseSunsetHolderContainer.sunsetTimeTv.text =
+            DateFormat.format("HH:mm", Date(currentWeather.sunset * 1000L)).toString()
     }
 }
