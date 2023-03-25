6dee97501b5a82f1ec015d92ee7ac47ce15b59cf
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-26 01:41:54 +0300

    Add searching of new location after text changed

--- app/src/main/java/cs/vsu/ru/application/view/fragment/AddNewLocationFragment.kt
+++ app/src/main/java/cs/vsu/ru/application/view/fragment/AddNewLocationFragment.kt
@@ -2,12 +2,13 @@ package cs.vsu.ru.application.view.fragment
 
 import android.content.Context.INPUT_METHOD_SERVICE
 import android.os.Bundle
-import android.view.KeyEvent
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -45,6 +46,16 @@ class AddNewLocationFragment : Fragment() {
         binding.fragmentFindLocationsBtn.setOnClickListener {
             handleSearchAction(it)
         }
+
+        binding.fragmentAddNewLocationInputEt.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                getLocationsByName(binding.fragmentAddNewLocationInputEt.text.toString())
+            }
+        })
 
         return binding.root
     }
