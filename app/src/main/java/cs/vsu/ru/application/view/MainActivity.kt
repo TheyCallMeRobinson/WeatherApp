f10b8b4efe7a99d85e5173e65c213e9d4cb6a618
Paul Robinson <alettertopaulrobinson0099@gmail.com>
2023-03-25 13:43:57 +0300

    Set navbar transparent

--- app/src/main/java/cs/vsu/ru/application/view/MainActivity.kt
+++ app/src/main/java/cs/vsu/ru/application/view/MainActivity.kt
@@ -2,6 +2,7 @@ package cs.vsu.ru.application.view
 
 import android.os.Bundle
 import android.view.View
+import android.view.WindowManager
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionScene
@@ -19,6 +20,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
 
         val actionBarDrawerToggle = object : ActionBarDrawerToggle(this, binding.root,
             R.string.drawer_open,
